35da1286b70232a8a0c71c731dc8da31c4e06d10
Johann Ly <johann.ly@outlook.fr>
2024-05-24 14:52:23 +0200

    Update js.js

--- e7/js.js
+++ e7/js.js
@@ -1,8 +1,8 @@
 var data
 
 function load_player() {
-  let request = await fetch('https://static.smilegatemegaport.com/gameRecord/epic7/epic7_user_world_eu.json');
-  let jsonData = await request.json();
+  let request = fetch('https://static.smilegatemegaport.com/gameRecord/epic7/epic7_user_world_eu.json');
+  let jsonData = request.json();
   data = await JSON.parse(jsonData)
 }
 window.onload = load_player;
