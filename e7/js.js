08f5343bbc3254cd060f8b57fe37aed079a51f04
Johann Ly <johann.ly@outlook.fr>
2024-05-25 20:22:24 +0200

    Update js.js

--- e7/js.js
+++ e7/js.js
@@ -6,19 +6,28 @@ var player_list = [
 	{region:'JPN',json_link:'https://static.smilegatemegaport.com/gameRecord/epic7/epic7_user_world_jpn.json'},
 	{region:'KOR',json_link:'https://static.smilegatemegaport.com/gameRecord/epic7/epic7_user_world_kor.json'}
 ]
+var player_loaded = false
 
 async function getJSON(ply_list) {
   return fetch(ply_list.json_link)
-    .then(response => response.json())
-    .then(json => {return json});
+    .then(response => {
+		if (response.ok) {
+			return response.json();
+		}
+		throw new Error('Error while fetching ${ply_list.region} player list');
+	})
+	.catch(error => {
+		console.log(error);
+	})
 }
 async function load_player() {
   for (i = 0; i < player_list.length; i++) {
 	  let region_json = await getJSON(player_list[i]);
 	  region_json = region_json.users;
-	  region_json = region_json.map(obj => ({ ...obj, region: player_list[i].region}))
-	  data = data.concat(region_json)
+	  region_json = region_json.map(obj => ({ ...obj, region: player_list[i].region}));
+	  data = data.concat(region_json);
   }
+  player_loaded = true;
 }
 
 window.onload = load_player;
